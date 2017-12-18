eba3b97bf11ee9c6f3c650091b647898b67b3532
Abdul Rasheed <narejo@gmail.com>
2017-12-18 13:13:17 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Udacity - Memory Game
+## About the Game
+The Memory Game Project is all about demonstrating mastery of HTML, CSS, and JavaScript. This requires building a complete browser-based card matching game (also known as Concentration).
+
+## Languages used
+1. HTML
+1. CSS
+1. Javascript
+1. (JQuery (Javascript Library))[https://jquery.com/]
+1. (Google Fonts)[https://fonts.google.com/]
+1. (Font Awesome)[http://fontawesome.io/]
+
+## How to run the game
+1. Clone the folder from Github
+1. cd to the relevant folder
+1. open `index.html` in your browser of choice (chrome is recommended for optimal experience)
+
+
+## References and help
+1. Youtube
+1. Udacity Javascript Tutorial
+1. Jquery documentation
+1. Font awesome documentation
---
+++ js/main.js
@@ -0,0 +1,141 @@
+var numbersList;
+var movesLeft;
+var time;
+var seconds;
+var minutes;
+var timerInterval;
+var text;
+
+$(document).ready(function() {
+  restart();
+
+  $('#restart').on('click', function() {
+    restart();
+  });
+
+  $('#restart_modal').on('click', function() {
+    $('.modal').addClass('modal_hide');
+    $('.top').removeClass('opacity');
+    $('.container').removeClass('opacity');
+    restart();
+  });
+
+  $('#close_modal').on('click', function() {
+    $('.modal').addClass('modal_hide');
+    $('.top').removeClass('opacity');
+    $('.container').removeClass('opacity');
+  });
+
+  // $('.card').on('click', function(index) {
+  //   $(this).html('<p>' + $(this).data('cardValue') + '</p>').addClass('selected');
+  //   setTimeout(function() {
+  //     checkMatch();
+  //   }, 500);
+  // });
+
+});
+
+// start/restart the game
+function restart() {
+  numbersList = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+
+  seconds = 0;
+  minutes = 0;
+
+  $('#cards').children().remove();
+
+  shuffle();
+
+  for (var i = 0; i < 16; i++) {
+    $('#cards').append('<div class="card" data-card-value="' + numbersList[i]+ '"></div>');
+  }
+
+  startClock();
+
+  movesLeft = 20;
+  $('#moves_left').html(movesLeft);
+
+  setRating();
+  $('#rating').html(text);
+
+  $('.card').on('click', function(index) {
+    $(this).html('<p>' + $(this).data('cardValue') + '</p>').addClass('selected');
+    setTimeout(function() {
+      checkMatch();
+    }, 500);
+  });
+
+};
+
+// this function will shuffle all the cards
+function shuffle() {
+  for (var i = 0; i < numbersList.length; i++ ) {
+    randomNumber = Math.round(Math.random()*i);
+    temp = numbersList[i];
+    numbersList[i] = numbersList[randomNumber];
+    numbersList[randomNumber] = temp;
+  }
+};
+
+// this function will check if last two clicked cards are a match
+function checkMatch() {
+  if ($('.selected').length == 2) {
+    movesLeft--;
+    $('#moves_left').html(movesLeft);
+
+    setRating();
+    $('#rating').html(text);
+
+    first = $('.selected').first().data('cardValue');
+    second = $('.selected').last().data('cardValue');
+
+    if (first == second) {
+      $('.selected').addClass('match');
+      checkWin();
+    } else {
+      $('.selected > p').remove();
+    }
+    $('.selected').removeClass('selected');
+  }
+}
+
+// to check if game won
+function checkWin() {
+  if($('.match').length == 16) {
+    stopClock();
+    $('#modal_message').html("Game Over you win");
+    $('#finish_time').html((minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds));
+    $('#final_rating').html(text);
+    $('.modal').removeClass('modal_hide');
+
+    $('.top').addClass('opacity');
+    $('.container').addClass('opacity');
+  }
+}
+
+function startClock() {
+  clearInterval(timerInterval);
+  timerInterval = setInterval(function(){
+
+    seconds += 1;
+    if (seconds > 60) {
+      minutes++;
+      seconds = 0;
+    }
+    $('#time').html((minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds));
+  }, 1000);
+  }
+
+function stopClock() {
+  clearInterval(timerInterval);
+}
+
+function setRating() {
+  if (movesLeft >= 10) {
+    text = '<i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>';
+  } else if (movesLeft >= 5) {
+    text = '<i class="fa fa-star" aria-hidden="true"></i><i class="fa fa-star" aria-hidden="true"></i>';
+  } else {
+    text = '<i class="fa fa-star" aria-hidden="true"></i>';
+  }
+}
